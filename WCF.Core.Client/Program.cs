1563d4c3517b852b954823453d1c3a2a048ce3d6
Lee <lishilei0523@163.com>
2018-04-19 20:17:13 +0800

    Autofac容器解析WCF完成

--- WCF.Core.Client/Program.cs
+++ WCF.Core.Client/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.ServiceModel.Toolkits;
 using WCF.IAppService.Interfaces;
 
@@ -8,15 +12,43 @@ namespace WCF.Core.Client
     {
         static void Main(string[] args)
         {
-            //信道工厂
-            ServiceProxy<IProductService> serviceProxy = new ServiceProxy<IProductService>();
-            IProductService productService = serviceProxy.Channel;
+            //初始化容器
+            IContainer container = InitContainer();
 
             //调用
+            IProductService productService = container.Resolve<IProductService>();
             string products = productService.GetProducts();
 
             Console.WriteLine(products);
+
+            container.Dispose();
             Console.ReadKey();
+        }
+
+        static IContainer InitContainer()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            Assembly wcfInterfaceAssembly = Assembly.Load("WCF.IAppService");
+
+            //获取WCF接口类型集
+            IEnumerable<Type> types = wcfInterfaceAssembly.GetTypes().Where(type => type.IsInterface);
+
+            //获取服务代理泛型类型
+            Type proxyGenericType = typeof(ServiceProxy<>);
+
+            //注册WCF接口
+            foreach (Type type in types)
+            {
+                Type proxyType = proxyGenericType.MakeGenericType(type);
+                PropertyInfo propChannel = proxyType.GetProperty(ServiceProxy.ChannelPropertyName, type);
+
+                builder.RegisterType(proxyType).OnRelease(proxy => ((IDisposable)proxy).Dispose());
+                builder.Register(container => propChannel.GetValue(container.Resolve(proxyType))).
+                    As(type).
+                    OnRelease(channel => channel.CloseChannel());
+            }
+
+            return builder.Build();
         }
     }
 }
